20dec3b9cb39c7c61d98abffd62d9e19fbb17f63
Nishant Chhillar <nchhillar2004@gmail.com>
2025-09-28 01:15:11 +0530

    added optimizations & security header in next.config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,60 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    // optimizations
+    compress: true,
+    poweredByHeader: false,
+
+    images: {
+        remotePatterns: 
+        process.env.NODE_ENV === "production" ? 
+            [
+                {
+                    protocol: 'https',
+                    hostname: '**.vercel.app', // TODO replace with prod hostname
+                    port: ''
+                }
+            ] :
+            [
+                {
+                    protocol: 'http',
+                    hostname: 'localhost',
+                    port: '3000',
+                }
+            ]
+    },
+
+    // security
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'X-Frame-Options',
+                        value: 'DENY',
+                    },
+                    {
+                        key: 'X-Content-Type-Options',
+                        value: 'nosniff',
+                    },
+                    {
+                        key: 'Referrer-Policy',
+                        value: 'origin-when-cross-origin',
+                    },
+                    {
+                        key: 'Permissions-Policy',
+                        value: 'geolocation=(self), camera=(), microphone=()',
+                    }
+                ]
+            }
+        ]
+    },
+
+    // experimental
+    experimental: {
+        optimizeCss: true
+    }
 };
 
 export default nextConfig;
